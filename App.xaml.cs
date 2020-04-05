9c03e28056d37db5291c90daf2471210e35a654b
Richard P. Gonzales <rpgonzo@gmail.com>
2020-04-05 00:36:48 -0500

    project init

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LiquidCtl_DOTNET_CPanel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace LiquidCtl_DOTNET_CPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
